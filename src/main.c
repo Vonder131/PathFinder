b47c581afd1b4fc3bbab536086ddb5f72cf5695c
Vonder131 <vlad.vonder.09@gmail.com>
2026-06-07 00:07:46 +0300

    Initial commit

---
+++ src/main.c
@@ -0,0 +1,81 @@
+﻿#include "raylib.h"
+#define DRAW_AREA_WIDTH 1278
+#define DRAW_AREA_HEIGHT 594
+#define CELL_SIZE 18
+
+Color textColor;
+
+Rectangle drawArea = { 0,0,DRAW_AREA_WIDTH,DRAW_AREA_HEIGHT };
+
+void drawCells();
+void Draw();
+Rectangle getCellFromMousePosition();
+bool MouseInDrawArea();
+
+int main()
+{
+    InitWindow(1280, 720, "Pathfinder visualizer");
+
+    while (!WindowShouldClose()) {
+        Draw();
+    }
+
+    CloseWindow();
+	return 0;
+}
+
+
+void drawCells() {
+    for (int i = 0; i < DRAW_AREA_WIDTH / CELL_SIZE; i++) {
+        DrawLine(i * CELL_SIZE, 0, i * CELL_SIZE, DRAW_AREA_HEIGHT, BLACK);
+    }
+
+    for (int i = 0; i < DRAW_AREA_HEIGHT / CELL_SIZE; i++) {
+        DrawLine(0, i * CELL_SIZE, DRAW_AREA_WIDTH, i * CELL_SIZE, BLACK);
+    }
+}
+
+void Draw() {
+    BeginDrawing();
+
+
+
+    DrawRectangleLinesEx(drawArea, 3, DARKBLUE);
+    ClearBackground(LIGHTGRAY);
+    drawCells();
+
+    DrawText(TextFormat("%d; %d", GetMouseX(), GetMouseY()), 200, DRAW_AREA_HEIGHT + 20, 12, textColor);
+    
+    if (MouseInDrawArea()) {
+        textColor = DARKGREEN;
+    }
+    else {
+        textColor = RED;
+    }
+
+    if (MouseInDrawArea())
+        DrawRectangleRec(getCellFromMousePosition(), RED);
+
+    EndDrawing();
+}
+
+Rectangle getCellFromMousePosition() {
+    Rectangle cell = { 0, 0,CELL_SIZE,CELL_SIZE };
+
+    while (cell.x < GetMouseX() - 18) {
+        cell.x += 18;
+    }
+
+    while (cell.y < GetMouseY() - 18) {
+        cell.y += 18;
+    }
+
+    return cell;
+}
+
+bool MouseInDrawArea() {
+    if (GetMouseX() < DRAW_AREA_WIDTH && GetMouseY() < DRAW_AREA_HEIGHT)
+        return true;
+    else
+        return false;
+}
